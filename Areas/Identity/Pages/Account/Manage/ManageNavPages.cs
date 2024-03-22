0600945055918dff953617ce61dc0df1131e565c
Kaloyan Sarafov <sarafovkaloyan@gmail.com>
2024-03-22 12:15:38 +0200

    Added company registration page in user management

--- Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -61,6 +61,8 @@ namespace  Calcpad.web.Areas.Identity.Pages.Account.Manage
         /// </summary>
         public static string TwoFactorAuthentication => "TwoFactorAuthentication";
 
+        public static string RegisterCompany => "RegisterCompany";
+        
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
@@ -109,6 +111,8 @@ namespace  Calcpad.web.Areas.Identity.Pages.Account.Manage
         /// </summary>
         public static string TwoFactorAuthenticationNavClass(ViewContext viewContext) => PageNavClass(viewContext, TwoFactorAuthentication);
 
+        public static string RegisterCompanyNavClass(ViewContext viewContext) => PageNavClass(viewContext, RegisterCompany);
+        
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
---
+++ Areas/Identity/Pages/Account/Manage/RegisterCompany.cshtml.cs
@@ -0,0 +1,76 @@
+﻿using System.Threading.Tasks;
+using Calcpad.web.Data.Models;
+using Calcpad.web.Data.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Calcpad.web.Areas.Identity.Pages.Account.Manage
+{
+    public class RegisterCompanyModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ICompanyService _companyService;
+
+        public RegisterCompanyModel(UserManager<ApplicationUser> userManager, ICompanyService companyService)
+        {
+            _userManager = userManager;
+            _companyService = companyService;
+        }
+
+        public class InputModel
+        {
+            public string Name { get; set; }
+            public string TaxRegistrationNumber { get; set; }
+            public bool VATRegistered { get; set; }
+            public int VATNumber { get; set; }
+            public string AccountablePerson { get; set; }
+            public string City { get; set; }
+            public string ZipCode { get; set; }
+            public string Address { get; set; }
+        }
+        
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            // Save the company details to the database here
+            //map the input model to the company model
+            var company = new Calcpad.web.Data.Models.Company
+            {
+                Name = Input.Name,
+                TaxRegistrationNumber = Input.TaxRegistrationNumber,
+                VATRegistered = Input.VATRegistered,
+                VATNumber = Input.VATNumber,
+                AccountablePerson = Input.AccountablePerson,
+                City = Input.City,
+                ZipCode = Input.ZipCode,
+                Address = Input.Address
+            };
+            
+            await _companyService.AddAsync(company);
+
+            StatusMessage = "Your company has been registered";
+            return RedirectToPage();
+        }
+    }
+}
