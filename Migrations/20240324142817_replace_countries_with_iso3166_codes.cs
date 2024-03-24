8a04cb43b520953b9912954bc4655552b7d5b054
Kaloyan Sarafov <sarafovkaloyan@gmail.com>
2024-03-24 16:37:25 +0200

    Added migrations from previous change

---
+++ Migrations/20240324142817_replace_countries_with_iso3166_codes.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Calcpad.web.Migrations
+{
+    /// <inheritdoc />
+    public partial class replace_countries_with_iso3166_codes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Companies_Countries_CountryId",
+                table: "Companies");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Companies_CountryId",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "CountryId",
+                table: "Companies");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Companies",
+                type: "nvarchar(3)",
+                maxLength: 3,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Companies");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CountryId",
+                table: "Companies",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(type: "nvarchar(2)", maxLength: 2, nullable: false),
+                    IsEU = table.Column<bool>(type: "bit", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(35)", maxLength: 35, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_CountryId",
+                table: "Companies",
+                column: "CountryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Companies_Countries_CountryId",
+                table: "Companies",
+                column: "CountryId",
+                principalTable: "Countries",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
