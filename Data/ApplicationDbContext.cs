1fccc22d99cd29f7377ca0fc2c17072af9933990
Kaloyan Sarafov <sarafovkaloyan@gmail.com>
2024-03-26 08:38:51 +0200

    Add Subscription.cs model to track active subscriptions

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -16,6 +16,7 @@ namespace Calcpad.web.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Worksheet> Worksheets { get; set; }
         public DbSet<SubscriptionPlan> SubscriptionPlans { get; set; }
+        public DbSet<Subscription> Subscriptions { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Topic> Topics { get; set; }
         public DbSet<Article> Articles { get; set; }
---
+++ Data/Models/Subscription.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Calcpad.web.Data.Models;
+
+public class Subscription
+{
+    public int Id { get; set; }
+    
+    public ApplicationUser User { get; set; }
+    
+    [Required]
+    public int PlanId { get; set; }
+    
+    public SubscriptionPlan Plan { get; set; }
+    
+    [Required]
+    bool isActive { get; set; }
+}
