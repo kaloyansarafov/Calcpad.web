7b019c35125f9f51e3bc11233ef244617efbdba9
Kaloyan Sarafov <sarafovkaloyan@gmail.com>
2024-04-15 09:51:49 +0300

    Commit migrations which were not tracked

---
+++ Migrations/20240410054708_change-months-to-days-subscription.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Calcpad.web.Migrations
+{
+    /// <inheritdoc />
+    public partial class changemonthstodayssubscription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Months",
+                table: "SubscriptionPlans",
+                newName: "Days");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Days",
+                table: "SubscriptionPlans",
+                newName: "Months");
+        }
+    }
+}
---
+++ Migrations/20240410054931_change-day-type-to-ushort.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Calcpad.web.Migrations
+{
+    /// <inheritdoc />
+    public partial class changedaytypetoushort : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Days",
+                table: "SubscriptionPlans",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "tinyint");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "Days",
+                table: "SubscriptionPlans",
+                type: "tinyint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
